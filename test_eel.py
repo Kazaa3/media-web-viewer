a949391ebc0efbca8e82698d6dc0d66aa9bda08f
kaaza3 <liquid@posteo.de>
2026-03-03 14:46:07 +0100

    Remove test scripts from root

--- test_eel.py
+++
@@ -1,8 +0,0 @@
-import eel
-import bottle
-
-@bottle.route('/media/<filepath:path>')
-def serve_media(filepath):
-    return bottle.static_file(filepath, root='./media')
-
-print("Bottle route added")
--- test_stream.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-print(subprocess.run(["ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", "media/sample.alac"], capture_output=True, text=True).stdout.strip())
